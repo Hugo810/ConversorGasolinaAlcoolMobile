eca97cf2dd138c3a26bdb0e3cc89386d92d84828
Hugo de Souza e silva <81842738+Hugo810@users.noreply.github.com>
2025-03-13 15:57:16 -0300

    Primeiro commit
    Primeiro commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/components/Title'
+import Form from './src/components/Form'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+     
+    <Title/>
+    <Form/>
+
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D3D3D3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
