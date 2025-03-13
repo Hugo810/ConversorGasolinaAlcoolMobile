bb7071436f31f4e122b24de9563e922e5172789a
Hugo de Souza e silva <81842738+Hugo810@users.noreply.github.com>
2025-03-13 16:00:32 -0300

    Add files via upload

---
+++ src/components/Form/ResultProporcao/style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    textTitle:{
+        color: "#ff0043",
+        fontSize: 24,
+        fontWeight: "bold"
+    }
+});
+
+export default styles
